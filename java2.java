d9d5690538e35e0acbf688c62a190ff1cefb260f
rabiaglc <rabiagulec2004@gmail.com>
2024-01-01 15:48:10 +0300

    Java Print
    Java Code Println

---
+++ java2.java
@@ -0,0 +1,33 @@
+public class java2 {
+    
+    public static void main(String[] args) {
+        //Javada değişken tipleri
+        /* 
+         * byte  
+         * short
+         * int
+         * long
+         * float  4byte 
+         * double 8 byte
+         * char karakter 2 byte
+         * boolean true or false 1 byte
+         */
+        int a;
+        a = 51;
+        int b = 41;
+        short s = 32;
+        double d = 3.14;
+        char c = 'A';
+        boolean g = true;
+        String rabia = "Rabia";
+
+
+        System.out.println(a);
+        System.out.println(s);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(g);
+        System.out.println(rabia);
+        System.out.println("value of b:  " + b);
+    }
+}
